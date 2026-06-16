a8a821cf98dbb88567afdb0c8788552075e3b28f
kwonsh77777 <kwonsh7777@gmail.com>
2026-06-16 14:09:20 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'travel-log-v3';
+const CACHE_NAME = 'travel-log-v4';
 const urlsToCache = [
   './',
   './index.html',
@@ -18,7 +18,7 @@ self.addEventListener('install', event => {
 
 // 데이터 요청(fetch) 가로채기
 self.addEventListener('fetch', event => {
-  // 💡 [수정] 구글 서버 통신(API)이나 POST 요청은 캐시를 무시하고 바로 인터넷으로 연결합니다.
+  // 구글 서버 통신(API)이나 POST 요청은 캐시를 무시하고 바로 인터넷으로 연결합니다.
   if (event.request.method !== 'GET' || event.request.url.includes('script.google.com')) {
     return; 
   }
@@ -26,7 +26,6 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
-        // 캐시에 있으면 반환, 없으면 네트워크 요청
         return response || fetch(event.request);
       })
   );
