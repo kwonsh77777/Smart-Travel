af2599711b488c8f0b06bdca886d8ac360bcdeff
kwonsh77777 <kwonsh7777@gmail.com>
2026-06-22 13:06:18 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'travel-log-v4';
+const CACHE_NAME = 'travel-log-v5'; // 버전을 올려 스마트폰에 강제 업데이트 명령
 const urlsToCache = [
   './',
   './index.html',
@@ -18,11 +18,9 @@ self.addEventListener('install', event => {
 
 // 데이터 요청(fetch) 가로채기
 self.addEventListener('fetch', event => {
-  // 구글 서버 통신(API)이나 POST 요청은 캐시를 무시하고 바로 인터넷으로 연결합니다.
   if (event.request.method !== 'GET' || event.request.url.includes('script.google.com')) {
     return; 
   }
-  
   event.respondWith(
     caches.match(event.request)
       .then(response => {
